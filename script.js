664c1a7ee8779cfb087f9e2eabd1e33ab2938d6f
Livia <livia.namba@gmail.com>
2023-05-17 23:41:08 -0300

    first commit

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ jQuery(document).ready(function($){
 
 	function addProduct() {
 		productId = productId + 1;
-		var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">Product Name</a></h3><span class="price">$25.99</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
+var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="https://www.winebrasil.com.br/media/product/1fa/vik-milla-cala-2018-323.png" alt="vinho"></a></div><div class="product-details"><h3><a href="#0">Vik Milla Cala 2018</a></h3><span class="price">R$ 399,99</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
 		cartList.prepend(productAdded);
 	}
 
